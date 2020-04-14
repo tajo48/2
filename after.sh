c09e183d4a78d03b3080bc269071e899f3c936a7
tajo48 <55502906+tajo48@users.noreply.github.com>
2020-04-14 23:53:37 +0200

    Add files via upload

---
+++ after.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+pacman -S --noconfirm dhcpcd grub xorg sudo dosfstools os-prober mtools
+# Set date time
+ln -sf /usr/share/zoneinfo/Europe/Warsaw /etc/localtime
+hwclock --systohc
+
+# Set locale to en_US.UTF-8 UTF-8
+echo "
+en_US.UTF-8 UTF-8
+pl_PL.UTF-8 UTF-8
+" >> /etc/locale.gen
+
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+
+# Set hostname
+echo "ARCH" >> /etc/hostname
+echo "
+127.0.0.1 localhost
+::1
+127.0.1.1 ARCH.localdomain  ARCH" >> /etc/hosts
+
+# Set root password
+passwd
+
+# Create new user
+useradd -U wheel,uucp,video,audio,storage,games,input,optical tajo48
+echo "%wheel ALL=(ALL) ALL $(cat /etc/sudoers)" > /etc/sudoers
+
+
+# Install bootloader
+mkdir /boot/grub
+grub-install --target=x86_64-efi /dev/sda
+grub-mkconfig -o /boot/grub/grub.cfg
---
+++ install.sh
@@ -0,0 +1,51 @@
+#! /bin/bash
+
+# Timectl
+timedatectl set-ntp true
+
+# Font
+loadkeys pl
+setfont Lat2-Terminus16.psfu.gz -m 8859-2
+
+# Setup the disk and partitions
+parted /dev/sda --script mklabel dos
+parted /dev/sda --script mkpart primary fat32 1MiB 300MiB #boot /dev/sda1
+parted /dev/sda --script mkpart primary ext4 300MiB 100% #home /dev/sda2
+parted /dev/sda --script set 2 boot on
+
+# Wipefs
+wipefs /dev/sda1
+wipefs /dev/sda2
+
+# Mkfs
+mkfs.ext4 /dev/sda1
+
+# Set up time
+timedatectl set-ntp true
+
+# Initate pacman keyring
+<<com
+pacman-key --init
+pacman-key --populate archlinux
+pacman-key --refresh-keys
+com
+
+# Mount the partitions
+mount /dev/sda1 /mnt
+mkdir /mnt/boot
+mkswap /dev/sda2
+swapon /dev/sda2
+
+# Install Arch Linux
+pacstrap /mnt base linux pacman
+
+# Generate fstab
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# Copy post-install system cinfiguration script to new /root
+    wget https://raw.githubusercontent.com/tajo48/2/master/after.sh -O /mnt/root/after.sh
+    chmod +x /mnt/root/after.sh
+
+# Chroot into new system
+arch-chroot /mnt /root/after.sh
+
