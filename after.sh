3270d0de08fb1f10bd2ae6c6c91587d2a891c817
tajo48 <55502906+tajo48@users.noreply.github.com>
2020-04-30 20:20:31 +0200

    Update after.sh

--- after.sh
+++ after.sh
@@ -33,9 +33,6 @@ echo "user pasword"
 passwd tajo48
 clear
 usermod -aG wheel,audio,video,optical,storage tajo48
-#sed -i '/# %wheel ALL=(ALL) ALL /s/^/jkuhfsjfhsdhflksdjhflksdhflksdhflksdhflksdhflkdhflksdhf/' /etc/sudoers
-#echo "%wheel ALL=(ALL) ALL $(cat /etc/sudoers)" > /etc/sudoers
-#sed -i /etc/sudoers -e "s/# %wheel ALL=(ALL) ALL/# %wheel ALL=(ALL) ALL'\n'%wheel ALL=(ALL) ALL/"
 
 # Install bootloader
 mkdir /boot/grub
