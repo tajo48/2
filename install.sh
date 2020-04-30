387426fa625a869bb4f96d2cce13aebe4c86f4d0
tajo48 <55502906+tajo48@users.noreply.github.com>
2020-04-30 19:57:57 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -43,7 +43,8 @@ pacstrap /mnt base linux pacman sudo
 genfstab -U /mnt >> /mnt/etc/fstab
 
 # Add sudo to wheel
-echo "%wheel ALL=(ALL) ALL $(cat /etc/sudoers)" > /etc/sudoers
+echo "%wheel ALL=(ALL) ALL 
+$(cat /etc/sudoers)" > /etc/sudoers
 
 # Copy post-install system cinfiguration script to new /root
     wget https://raw.githubusercontent.com/tajo48/2/master/after.sh -O /mnt/root/after.sh
