c79d147499344561cd2d073cc3f3a46982d75d2e
tajo48 <55502906+tajo48@users.noreply.github.com>
2020-05-02 13:51:25 +0200

    Update after.sh

--- after.sh
+++ after.sh
@@ -50,5 +50,5 @@ cd /usr/src
 git clone git://git.suckless.org/dwm
 git clone git://git.suckless.org/st
 git clone git://git.suckless.org/dmenu
-make clean install
+#make clean install
 #echo "exec dwm $(cat .xinitrc)" > .xinitrc
