2cb79a964c46aff1ade8dc92b905847c3bd4cf06
tajo48 <55502906+tajo48@users.noreply.github.com>
2020-05-02 20:27:55 +0200

    Update after.sh

--- after.sh
+++ after.sh
@@ -40,6 +40,6 @@ usermod -aG wheel,audio,video,optical,storage,users tajo48
 grub-install /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
 
-#qtile wm
-pacman -S --noconfirm qtile feh firefox rxvt-unicode dmenu
-echo "exec qtile" >> ~/.xinitrc
+#i3 wm
+pacman -S --noconfirm i3 feh firefox rxvt-unicode dmenu
+echo "exec i3" >> ~/.xinitrc
