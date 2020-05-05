5387b0e0b1fd274a2817998a58efed8cb9fdef24
tajo48 <55502906+tajo48@users.noreply.github.com>
2020-05-05 21:28:09 +0200

    Update after.sh

--- after.sh
+++ after.sh
@@ -39,10 +39,12 @@ grub-mkconfig -o /boot/grub/grub.cfg
 #i3 wm
 pacman -S --noconfirm i3 feh firefox rxvt-unicode rofi neofetch guake
 echo "exec i3" >> ~/.xinitrc
+mkdir /root/.config
+mkdir /root/.config/i3
 #curl https://raw.githubusercontent.com/tajo48/2/master/wallpaper.jpg -O a.jpg
-#wget https://raw.githubusercontent.com/tajo48/2/master/config -O .config/i3/config
+wget https://raw.githubusercontent.com/tajo48/2/master/config -O /root/.config/i3/config
 #startx
 #killall i3
-#sed '4 i TEST123TEST123' ~/.config/i3/config
+sed '4 i TEST123TEST123' /root/.config/i3/config
 #neofetch
-#cat ~/.config/i3/config
+cat ~/.config/i3/config
