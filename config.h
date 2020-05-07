b8430ca34b503d5fdb6e80b780bda3dbd2929692
tajo48 <55502906+tajo48@users.noreply.github.com>
2020-05-08 01:29:45 +0200

    Update config.h

--- config.h
+++ config.h
@@ -59,7 +59,7 @@ static const Layout layouts[] = {
 /* commands */
 //static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 //static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL }; 
-static const char dmenumon[2]  = { "st", NULL };
+static const char dmenumon[]  = { "st", NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *printcmd[] = { "scrot", NULL };
 //static const char *raisevolumecmd[] = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "+10", NULL};
