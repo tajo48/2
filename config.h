853165897db54b548b599ecc62fbe68e6e432ae8
tajo48 <55502906+tajo48@users.noreply.github.com>
2020-05-08 22:02:59 +0200

    Update config.h

--- config.h
+++ config.h
@@ -61,13 +61,13 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of rofimenu, manipulated in spawn() */
 //static const char *rofimenu[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *roficmd[]  = { "rofi","-show","run", NULL };
-static const char *windowcmd[]  = { "rofi","-show","window", NULL };
+//static const char *windowcmd[]  = { "rofi","-show","window", NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = roficmd } },
-	{ ALTKEY,                       XK_Tab,    spawn,          {.v = windowcmd } },	
+//	{ ALTKEY,                       XK_Tab,    spawn,          {.v = windowcmd } },	
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
