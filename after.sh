04ebc604ccfec5cbedc9d39fcb4cb0f5cc50789a
tajo48 <55502906+tajo48@users.noreply.github.com>
2020-05-08 02:41:47 +0200

    Update after.sh

--- after.sh
+++ after.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 #programs
-pacman -S --noconfirm grub os-prober dmenu mtools dhcpcd vim git make wget xorg-server pulseaudio xorg-xinit curl tar libxft mc fakeroot binutils patch pkgconf base-devel
+pacman -S --noconfirm grub os-prober dmenu mtools dhcpcd vim git make alsa-firmware wget xorg-server pulseaudio xorg-xinit curl tar libxft mc fakeroot binutils patch pkgconf base-devel
 # Set date time
 ln -sf /usr/share/zoneinfo/Europe/Warsaw /etc/localtime
 hwclock --systohc
