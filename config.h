a73857bd2c3d165d20d0fc72777e190f88bdf3a3
tajo48 <55502906+tajo48@users.noreply.github.com>
2020-05-08 22:09:25 +0200

    Update config.h

--- config.h
+++ config.h
@@ -58,7 +58,7 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static char dmenumon[2] = "0"; /* component of rofimenu, manipulated in spawn() */
+static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 //static const char *rofimenu[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *roficmd[]  = { "rofi","-show","run", NULL };
 //static const char *windowcmd[]  = { "rofi","-show","window", NULL };
