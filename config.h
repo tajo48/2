4748e47b6fa2454b07b4aded5b26a9a14e0a2036
tajo48 <55502906+tajo48@users.noreply.github.com>
2020-05-08 21:39:28 +0200

    Update config.h

--- config.h
+++ config.h
@@ -7,11 +7,11 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#fc4f6f";
+static const char col_gray2[]       = "#ffd500";
+static const char col_gray3[]       = "#95ffaa";
+static const char col_gray4[]       = "#54f7f0";
+static const char col_cyan[]        = "#e399ff";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
