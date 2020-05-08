6cfecd885d691cf804d8e394e2ef80752cac454b
tajo48 <55502906+tajo48@users.noreply.github.com>
2020-05-08 02:14:57 +0200

    Update config.h

--- config.h
+++ config.h
@@ -6,7 +6,6 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=9" };
-/*static const char dmenufont[]       = "monospace:size=9";*/
 static const int gappx              = 10;
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -56,9 +55,7 @@ static const Layout layouts[] = {
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
-/* commands
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };*/ 
+/* commands*/ 
 static const char *dmenucmd[]  = { "st", NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *printcmd[] = { "scrot", NULL };
