8e18dfed728d10f8147ea5b8dcd6b934b2f746e2
tajo48 <55502906+tajo48@users.noreply.github.com>
2020-05-09 02:07:27 +0200

    Update after.sh

--- after.sh
+++ after.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 #programs
-pacman -S --noconfirm alsa-utils netctl grub os-prober mtools dialog wpa_supplicant dhcpcd vim git make alsa-firmware wget xorg-server pulseaudio xorg-xinit curl tar libxft mc fakeroot binutils patch pkgconf base-devel
+pacman -S --noconfirm alsa-utils netctl grub os-prober mtools dialog wpa_supplicant dhcpcd vim git make alsa-firmware wget xorg-server pulseaudio xorg-xinit curl tar libxft ranger fakeroot binutils patch pkgconf base-devel
 #obs-studio
 
 # Set date time
