c6b562724840699bd7288e9229442d01ffab62ac
tajo48 <55502906+tajo48@users.noreply.github.com>
2020-06-01 00:27:47 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,18 +5,10 @@ loadkeys pl
 setfont Lat2-Terminus16.psfu.gz -m 8859-2
 
 # Setup the disk and partitions
-parted /dev/sda --script mklabel msdos
-parted /dev/sda --script mkpart primary linux-swap 1MiB 300MiB #boot /dev/sda1
-parted /dev/sda --script mkpart primary ext4 300MiB 100% #root /dev/sda2
-parted /dev/sda --script set 2 boot on
 
 # Wipefs
-wipefs /dev/sda1
-wipefs /dev/sda2
 
 # Mkfs
-mkfs.ext4 /dev/sda2
-mkswap /dev/sda1
 
 # Set up time
 timedatectl set-ntp true
@@ -40,7 +32,7 @@ pacstrap /mnt base linux pacman sudo
 genfstab -U /mnt >> /mnt/etc/fstab
 
 # Copy post-install system cinfiguration script to new /root
-    wget https://raw.githubusercontent.com/tajo48/2/master/after.sh -O /mnt/root/after.sh
+    wget https://raw.githubusercontent.com/tajo48/2/uefi/after.sh -O /mnt/root/after.sh
     chmod +x /mnt/root/after.sh
 
 # Chroot into new system
