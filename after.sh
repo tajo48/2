e3a7fb9dcf23e72af1ed4d68cacee6b41e013bcf
tajo48 <55502906+tajo48@users.noreply.github.com>
2020-06-02 22:39:19 +0200

    Update after.sh

--- after.sh
+++ after.sh
@@ -39,7 +39,6 @@ mkdir /boot/efi
 mount /dev/sda1 /boot/efi
 grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot/efi
 grub-mkconfig -o /boot/grub/grub.cfg
-read -p "press enter"
 
 #makepkg in root
 rm /usr/bin/makepkg
