18ca126baeeab6967171af9d2785a16b8f985064
tajo48 <55502906+tajo48@users.noreply.github.com>
2020-06-02 23:03:11 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ parted /dev/sda --script mklabel gpt
 parted /dev/sda --script mkpart primary ext4 1MiB 512MiB #boot /dev/sda1
 parted /dev/sda --script mkpart primary ext4 512MiB 100% #root /dev/sda2
 set 1 esp on
+
 #mkfs
 mkfs.fat -F32 /dev/sda1
 mkfs.ext4 /dev/sda2
