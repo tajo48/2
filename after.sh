42e5c7424b28dda4adb415964611630e77aa2409
tajo48 <55502906+tajo48@users.noreply.github.com>
2020-06-12 19:42:37 +0200

    Update after.sh

--- after.sh
+++ after.sh
@@ -49,6 +49,7 @@ rm /usr/bin/makepkg
 wget https://raw.githubusercontent.com/tajo48/ARCH-files/master/makepkg -O /usr/bin/makepkg
 chmod +x /usr/bin/makepkg
 
+<<COM2137
 #Xmonad
 pacman -S --noconfirm xmobar firefox feh termite xmonad xmonad-contrib conky
 #https://aur.archlinux.org/cgit/aur.git/snapshot/brave.tar.gz
@@ -97,3 +98,48 @@ wget https://raw.githubusercontent.com/tajo48/ARCH-files/master/xmonad.hs -O /ho
 #echo "feh --bg-fill /home/tajo48/photos/wallpaper.jpg
 #setxkbmap -layout 'pl'" >> ~/.xprofile
 #replace with ~/.xmonad
+COM2137
+
+###DWM part
+#wallpaper
+mkdir /home/tajo48/photos
+wget https://raw.githubusercontent.com/tajo48/ARCH-files/master/photos/wallpaper.jpg -O /home/tajo48/photos/wallpaper.jpg
+#dwm try
+cd /home/tajo48
+echo "feh --bg-fill /home/tajo48/photos/wallpaper.jpg
+setxkbmap -layout 'pl'
+exec dwm" >> ~/.xinitrc
+pacman -S --noconfirm feh firefox rxvt-unicode neofetch
+#download (almost temporary)
+git clone https://git.suckless.org/dwm/
+git clone https://git.suckless.org/st/
+git clone https://git.suckless.org/dmenu/
+###DWM
+#wgetpatch (temporary)
+cd /home/tajo48/dwm
+wget https://dwm.suckless.org/patches/fakefullscreen/dwm-fakefullscreen-20170508-ceac8c9.diff
+wget https://dwm.suckless.org/patches/pertag/dwm-pertag-6.2.diff
+wget https://dwm.suckless.org/patches/uselessgap/dwm-uselessgap-6.2.diff
+wget https://dwm.suckless.org/patches/movestack/dwm-movestack-6.1.diff
+#patch (temporary)
+cp dwm.c dwm.c.orig
+patch < dwm-fakefullscreen-20170508-ceac8c9.diff
+patch < dwm-pertag-6.2.diff
+patch < dwm-uselessgap-6.2.diff
+patch < dwm-movestack-6.1.diff
+###DMENU
+#wgetpatch (temporary)
+cd /home/tajo48/dmenu
+wget https://tools.suckless.org/dmenu/patches/center/dmenu-center-4.8.diff
+#patch (temporary)
+patch < dmenu-center-4.8.diff
+###MKPKG
+#cp config.h config.def.h
+#makekpkg
+cd /home/tajo48/st
+make clean install
+cd /home/tajo48/dmenu
+make clean install
+cd /home/tajo48/dwm
+make clean install
+startx
