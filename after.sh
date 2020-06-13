4564563bb9dbe19a26146e48cc974f77a14783e1
tajo48 <55502906+tajo48@users.noreply.github.com>
2020-06-14 00:05:09 +0200

    Update after.sh

--- after.sh
+++ after.sh
@@ -65,7 +65,6 @@ exec dwm" >> /home/tajo48/.xinitrc
 pacman -S --noconfirm feh firefox rxvt-unicode
 mkdir /home/tajo48/photos
 wget https://raw.githubusercontent.com/tajo48/ARCH-files/master/photos/wallpaper.jpg -O /home/tajo48/photos/wallpaper.jpg
-cd /home/tajo48
 git clone https://github.com/tajo48/ARCH-builds.git
 
 
