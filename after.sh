436f7a799c9a1425988ec8ca63ebc23aaf43dbf8
tajo48 <55502906+tajo48@users.noreply.github.com>
2020-06-14 00:02:43 +0200

    Update after.sh

--- after.sh
+++ after.sh
@@ -63,52 +63,8 @@ setxkbmap -layout 'pl'
 exec xcompmgr -c &
 exec dwm" >> /home/tajo48/.xinitrc
 pacman -S --noconfirm feh firefox rxvt-unicode
-#download (almost temporary)
-git clone https://git.suckless.org/dwm/
-git clone https://git.suckless.org/st/
-git clone https://git.suckless.org/dmenu/
-###DWM
-#wgetpatch (temporary)
-cd /home/tajo48/dwm
-wget https://dwm.suckless.org/patches/fakefullscreen/dwm-fakefullscreen-20170508-ceac8c9.diff
-wget https://dwm.suckless.org/patches/pertag/dwm-pertag-6.2.diff
-wget https://dwm.suckless.org/patches/uselessgap/dwm-uselessgap-6.2.diff
-wget https://dwm.suckless.org/patches/movestack/dwm-movestack-6.1.diff
-wget https://dwm.suckless.org/patches/scratchpad/dwm-scratchpad-6.2.diff
-wget https://dwm.suckless.org/patches/centeredmaster/dwm-centeredmaster-6.1.diff
-
-
-#patch (temporary)
-cp dwm.c dwm.c.orig
-patch < dwm-scratchpad-6.2.diff
-patch < dwm-fakefullscreen-20170508-ceac8c9.diff
-patch < dwm-pertag-6.2.diff
-patch < dwm-uselessgap-6.2.diff
-patch < dwm-movestack-6.1.diff
-patch < dwm-centeredmaster-6.1.diff
-
-
-
-###DMENU
-#wgetpatch (temporary)
-cd /home/tajo48/dmenu
-wget https://tools.suckless.org/dmenu/patches/center/dmenu-center-4.8.diff
-wget https://tools.suckless.org/dmenu/patches/border/dmenu-border-4.9.diff
-#patch (temporary)
-patch < dmenu-center-4.8.diff
-patch < dmenu-border-4.9.diff
-sed -i '/static unsigned int lines/ s/0/15/' /home/tajo48/dmenu/config.def.h
-
-###ST
-cd /home/tajo48/st
-wget https://st.suckless.org/patches/alpha/st-alpha-0.8.2.diff
-wget https://st.suckless.org/patches/scrollback/st-scrollback-20200419-72e3f6c.diff
-wget https://st.suckless.org/patches/clipboard/st-clipboard-0.8.3.diff
-
-#patch
-patch < st-scrollback-20200419-72e3f6c.diff
-patch < st-alpha-0.8.2.diff
-patch < st-clipboard-0.8.3.diff
+cd /home/tajo48
+git clone https://github.com/tajo48/ARCH-builds.git
 
 ###MKPKG
 #cp config.h config.def.h
