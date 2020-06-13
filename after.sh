35249e9c9174876f1e4e41fad534ce84765b3001
tajo48 <55502906+tajo48@users.noreply.github.com>
2020-06-14 00:04:01 +0200

    Update after.sh

--- after.sh
+++ after.sh
@@ -63,8 +63,11 @@ setxkbmap -layout 'pl'
 exec xcompmgr -c &
 exec dwm" >> /home/tajo48/.xinitrc
 pacman -S --noconfirm feh firefox rxvt-unicode
+mkdir /home/tajo48/photos
+wget https://raw.githubusercontent.com/tajo48/ARCH-files/master/photos/wallpaper.jpg -O /home/tajo48/photos/wallpaper.jpg
 cd /home/tajo48
 git clone https://github.com/tajo48/ARCH-builds.git
+
 
 ###MKPKG
 #cp config.h config.def.h
