c055b0359107c378b1d02938fee63690078b5eb7
tajo48 <55502906+tajo48@users.noreply.github.com>
2020-09-20 20:35:23 +0200

    Update after.sh

--- after.sh
+++ after.sh
@@ -59,6 +59,8 @@ su tajo48 << 'NO'
 wal -i /home/tajo48/ARCH-files/photos
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 NO
 
 #dwm try
