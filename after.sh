6d00f5433fadd57bc51ac3b12c537a0e983833e4
tajo48 <55502906+tajo48@users.noreply.github.com>
2020-10-18 14:08:39 +0200

    Update after.sh

--- after.sh
+++ after.sh
@@ -4,7 +4,7 @@
 pacman -S --noconfirm alsa-utils netctl mtools dialog wpa_supplicant dhcpcd git neofetch mc xclip clipmenu rtorrent cura nano openssh notepadqq tmux arandr slock
 pacman -S --noconfirm obs-studio blender bashtop calc virtualbox virtualbox-host-modules-arch vlc youtube-dl htop tar p7zip xcompmgr audacity gimp cmus discord nautilus
 pacman -S --noconfirm vifm make alsa-firmware wget xorg xorg-server xorg-xinit curl libxft fakeroot binutils gvim neovim patch pkgconf base-devel python-pywal pulseaudio alsa
-pacman -S --noconfirm firefox zsh feh virtualbox-guest-utils python-pynvim
+pacman -S --noconfirm firefox zsh feh virtualbox-guest-utils python-pynvim nodejs yarn
 
 
 # Set date time
